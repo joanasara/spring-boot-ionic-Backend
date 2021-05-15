bbe6c5db21d590f8c41658b842e7675ef782ca1b
joana leite <joana.leite@aluno.unifametro.edu.br>
2021-05-15 15:53:35 -0300

    Adicinado Pedido POST

--- src/main/java/com/springboot/api/services/PedidoService.java
+++ src/main/java/com/springboot/api/services/PedidoService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import com.springboot.api.domain.Pedido;
 
 import com.springboot.api.repository.PedidoRepository;
@@ -16,11 +17,15 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 
-	public Pedido buscar(Integer id) {
+	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 
 	}
+	public Pedido insert(Pedido obj) {
+		 obj.setId(null);
+		 return repo.save(obj);
+	 }
 
 }
