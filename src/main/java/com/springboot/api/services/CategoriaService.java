0c9265874c18cdf06f9ededb0fc34a5bca8ef28f
joana leite <joana.leite@aluno.unifametro.edu.br>
2021-05-15 16:44:31 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/springboot/api/services/CategoriaService.java
+++ src/main/java/com/springboot/api/services/CategoriaService.java
@@ -1,12 +1,15 @@
 package com.springboot.api.services;
 
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.springboot.api.domain.Categoria;
 import com.springboot.api.repository.CategoriaRepository;
+import com.springboot.api.services.exceptions.DataIntegrityExcepetion;
 import com.springboot.api.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -18,17 +21,28 @@ public class CategoriaService {
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
 	}
-  
-	 public Categoria insert(Categoria obj) {
-		 obj.setId(null);
-		 return repo.save(obj);
-	 }
-	 
-	 public Categoria update(Categoria obj) {
-		 find(obj.getId());
-		 return repo.save(obj);
-	 }
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+            throw new  DataIntegrityExcepetion("não e possivel Ecluir categoria que possui produtor");
+		}
+	}
+
 }
---
+++ src/main/java/com/springboot/api/services/exceptions/DataIntegrityExcepetion.java
@@ -0,0 +1,16 @@
+package com.springboot.api.services.exceptions;
+
+public class DataIntegrityExcepetion extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	
+	public DataIntegrityExcepetion(String msg) {
+		super(msg);
+	}
+	
+	public DataIntegrityExcepetion (String msg, Throwable cause) {
+		
+		super(msg,cause);
+		
+	}
+}
